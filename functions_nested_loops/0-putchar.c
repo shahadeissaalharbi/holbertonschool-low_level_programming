eb0422924b39d3ddc9ebd6b92fab83e9bdeb5083
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-02-25 18:14:25 +0000

    Add 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - prints _putchar followed by a new line
-*
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _putchar('_');
@@ -18,3 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+
