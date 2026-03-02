002f765aa63b5701f517604c9661210ebbf973a4
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-03-02 18:00:44 +0000

    Add main.h and fix reset_to_98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,10 @@
 /**
  * reset_to_98 - updates the value pointed to by n to 98
  * @n: pointer to the int to update
+ *
+ * Return: void
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
