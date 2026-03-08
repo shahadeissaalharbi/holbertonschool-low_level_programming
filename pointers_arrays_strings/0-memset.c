cab435df4fb4dd54a93f3a33a9567f8227625dfd
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-03-08 18:06:27 +0000

    Fix betty style 0-memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: the constant byte
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < n)
-    {
-        s[i] = b;
-        i++;
-    }
-    return (s);
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
