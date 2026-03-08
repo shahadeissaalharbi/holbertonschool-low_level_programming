72abc602565a9d4ef6f5d749b8605bfc262b8697
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-03-08 18:52:13 +0000

    Fix 5-sqrt_recursion remove static keyword

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,17 +1,17 @@
 /**
- * helper - checks if i is the square root of n
- * @n: the number
- * @i: the current value to check
+ * _sqrt_helper - helper function to find square root
+ * @n: the target number
+ * @i: current value to check
  *
  * Return: square root of n or -1
  */
-static int helper(int n, int i)
+int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	if (i * i > n)
 		return (-1);
-	return (helper(n, i + 1));
+	return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (helper(n, 0));
+	return (_sqrt_helper(n, 0));
 }
