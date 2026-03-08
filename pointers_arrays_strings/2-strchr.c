be2d33594f25643e2b64a021b16a35e9a27a6bbb
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-03-08 18:11:41 +0000

    Fix 2-strchr add stddef.h for NULL

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -9,7 +10,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
 	while (s[i] != '\0')
