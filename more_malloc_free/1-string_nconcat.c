06bb126623b8b3255d7e8005727c369c731d0509
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-03-29 12:57:00 +0000

    Fix strlen not allowed in string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * string_nconcat - concatenates two strings
@@ -21,8 +20,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	if (n >= len2)
 		n = len2;
