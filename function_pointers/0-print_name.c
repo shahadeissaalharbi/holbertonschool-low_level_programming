d603aa776bc9d6c8f30b2e3ba06c762d2f0d8282
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-04-11 17:53:15 +0000

    Add task 0 - print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name using a function pointer
  * @name: name of the person
- * @f: pointer to function
+ * @f: pointer to function that takes a char * and returns nothing
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
-    f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
