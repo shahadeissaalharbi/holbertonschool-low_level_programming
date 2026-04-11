6bb19d407fe2b60956247b99fbcc26fb7646858f
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-04-11 18:04:20 +0000

    Fix 3-get_op_func.c stray characters

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -26,32 +26,4 @@ int (*get_op_func(char *s))(int, int)
 		i++;
 	}
 	return (NULL);
-}1~#include <string.h>
-#include "3-calc.h"
-
-/**
- * get_op_func - selects the correct function for the operator
- * @s: the operator string
- * Return: pointer to the corresponding function, or NULL
- */
-int (*get_op_func(char *s))(int, int)
-{
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
-
-	i = 0;
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
 }
