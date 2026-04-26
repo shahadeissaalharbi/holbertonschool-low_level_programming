f64ebc0723f439a130ee409c412bd45bc13154bc
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-04-26 11:19:06 +0000

    Fix djb2 prototype in header and add Betty docs

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -5,12 +5,6 @@
 #include <string.h>
 #include <stdio.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the list
- */
 typedef struct hash_node_s
 {
 	char *key;
@@ -18,18 +12,13 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- */
 typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
